ribalda/ethercat

Allow the application to explicitly process slave requests cyclically from the applications context.  ecrt_master_exec_slave_requests() can take some time to return so should be called from a separate thread from you realtime application.
For example (in RTAI) create a soft rt thread that loops, waiting on the main
RT thread to complete its cycle before calling ecrt_master_exec_slave_requests()

master/ioctl.h
@@ -169,6 +169,8 @@
 #define EC_IOCTL_FOE_REQUEST_READ     EC_IOWR(0x68, ec_ioctl_foe_request_t)
 #define EC_IOCTL_FOE_REQUEST_WRITE    EC_IOWR(0x69, ec_ioctl_foe_request_t)
 #define EC_IOCTL_FOE_REQUEST_DATA     EC_IOWR(0x6a, ec_ioctl_foe_request_t)
+#define EC_IOCTL_RT_SLAVE_REQUESTS     EC_IOW(0x6b, uint32_t)
+#define EC_IOCTL_EXEC_SLAVE_REQUESTS    EC_IO(0x6c)
 
 /*****************************************************************************/
 

cerna/etherlab

master/ioctl.h
@@ -165,6 +165,8 @@
 #define EC_IOCTL_FOE_REQUEST_READ     EC_IOWR(0x69, ec_ioctl_foe_request_t)
 #define EC_IOCTL_FOE_REQUEST_WRITE    EC_IOWR(0x6a, ec_ioctl_foe_request_t)
 #define EC_IOCTL_FOE_REQUEST_DATA     EC_IOWR(0x6b, ec_ioctl_foe_request_t)
+#define EC_IOCTL_RT_SLAVE_REQUESTS     EC_IOW(0x6c, uint32_t)
+#define EC_IOCTL_EXEC_SLAVE_REQUESTS    EC_IO(0x6d)
 
 /*****************************************************************************/
 